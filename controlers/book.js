433614705b8290a02702003c0b9bc2f9561d988c
racha <rachabnabid@gmail.com>
2022-08-19 11:49:06 +0100

    controlers book

---
+++ controlers/book.js
@@ -0,0 +1,56 @@
+const Book = require('../models/Book');
+
+exports.createBook = async (req, res)=>{
+  try {
+    await Book.create(req.body);
+    res.send({message: 'Book created'})
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'Some error occured'
+    });
+  }
+}
+
+exports.getBookById = async (req, res)=>{
+  try {
+    const book = await Book.findById(req.params.idBook);
+    res.send(book);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'Some error occured'
+    }); 
+  }
+}
+
+exports.getBooks = async (req, res)=>{
+  try {
+    const books = await Book.find();
+    res.send(books);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'Some error occured'
+    }); 
+  }
+}
+
+exports.updateBook = async (req, res)=>{
+  try {
+    await Book.findByIdAndUpdate(req.params.idBook, req.body);
+    res.send({message: 'Book updated'})
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'Some error occured'
+    }); 
+  }
+}
+
+exports.deleteBook = async (req, res)=>{
+  try {
+    await Book.findByIdAndRemove(req.params.idBook);
+    res.send({message: 'Book deleted'})
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'Some error occured'
+    }); 
+  }
+}
