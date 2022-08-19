b8cba4601611f8e1caed649879d09319260087cc
racha <rachabnabid@gmail.com>
2022-08-19 14:35:46 +0100

    upload

--- models/user.js
+++ models/user.js
@@ -21,7 +21,6 @@ const UserSchema = new Schema({
   role: {
     type: String,
     required: [true, 'Role is required'],
-    delault: 'Admin'
   }
 },
 {
--- routes/Api.customer.js
+++ routes/Api.customer.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const folder = path.resolve('./uploads')
+    cb(null, folder)
+  },
+  filename: (req, file, cb) => {
+    const fileExtension = path.extname(file.originalname)
+    const filename = Date.now() + fileExtension
+    cb(null, filename)
+  }
+})
+
+function fileFilter(req, file, cb) {
+  const fileExtension = path.extname(file.originalname)
+  const acceptedExtensions = ['.pdf']
+
+  cb(null, acceptedExtensions.includes(fileExtension))
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter, limits: {fieldSize: 25*1024*1024} })
 
 const { createCustomer, getCustomers, getCustomerById, deleteCustomer, updateCustomer } = require('../controlers/customer')
 
-router.post('/Customer', passport.authenticate('bearer', { session: false }), createCustomer);
+router.post('/Customer', [passport.authenticate('bearer', { session: false }), upload.single('file')], createCustomer);
 router.get('/Customers', passport.authenticate('bearer', { session: false }), getCustomers);
 router.get('Customer/:idCustomer', passport.authenticate('bearer', { session: false }), getCustomerById);
-router.put('Customer/:idCustomer', passport.authenticate('bearer', { session: false }), updateCustomer);
+router.put('Customer/:idCustomer', [passport.authenticate('bearer', { session: false }), upload.single('file')], updateCustomer);
 router.delete('Customer/:idCustomer', passport.authenticate('bearer', { session: false }), deleteCustomer);
 
 
