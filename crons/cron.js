5b67f8c3552ddd4d831da3fa6e804945445f05fc
racha <rachabnabid@gmail.com>
2022-10-07 15:55:58 +0100

    modif

--- crons/cron.js
+++ crons/cron.js
@@ -3,4 +3,5 @@ const cron = require('node-cron');
 
 cron.schedule('0 0 1 * *', () => {
   console.log('reset the number of downloads on the first day of each month');
+  customer.countDownload == 0;
 });
