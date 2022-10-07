f36f76557701b14c80e3586aa7b230459528db9e
racha <rachabnabid@gmail.com>
2022-10-07 16:09:13 +0100

    modif

--- crons/cron.js
+++ crons/cron.js
@@ -1,7 +1,12 @@
 const cron = require('node-cron');
+const Customer = require('../models/Customer')
 
 
 cron.schedule('0 0 1 * *', () => {
   console.log('reset the number of downloads on the first day of each month');
-  customer.countDownload == 0;
-});
+  // customer.countDownload == 0;
+  const customer = Customer.findByIdAndUpdate(req.user.customerId)
+  if(customer.countDownload ==! 0){
+    Customer.findByIdAndUpdate(req.user.customerId, {countDownload: 0}, {new: true})
+};
+})
